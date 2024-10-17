3f9fcf7e61c8ecdf0c4bd249e9cd8dc7431d71f0
r_s_higa <s_higa@r-jc.jp>
2024-10-17 14:29:58 +0900

    指摘対応 https://github.com/diesel141/htmlGenalouder/pull/3

--- app/chatgpt/api.py
+++ app/chatgpt/api.py
@@ -1,47 +1,45 @@
-import openai
+import os
 from openai import OpenAI
+from dotenv import load_dotenv
 import deepl
 
-client = OpenAI( api_key = "" )
-model_name = "gpt-3.5-turbo"
+load_dotenv()
+translator = deepl.Translator(os.environ.get("DEEPL_API_KEY"))
+client = OpenAI( api_key = os.environ.get("OPENAI_API_KEY"))
 
-DEEPL_API_KEY = ""
-source_lang = "JA"
-target_lang = "EN-US"
-translator = deepl.Translator(DEEPL_API_KEY)
+model_name = os.environ.get("CHATGPT_MODEL_NAME")
+source_lang = os.environ.get("VOICE_SOURCE_LANG")
+target_lang = os.environ.get("CHATGPT_INPUT_LANG")
 
 def get_html_from_chat_gpt(request):
 
-    print(request)
-
-    # syatem prompt
+    # system prompt
     system_prompt1 = "Webブラウザで表示したい画面構成を指定した要求がされます。\n"
     system_prompt2 = "使用するブラウザはGoogle Chromeです。\n"
-    system_prompt3 = "要求を満たす画面構成をHTML形式で返してください。\n"
+    system_prompt3 = "要求を満たす画面構成をHTML形式、かつHTMLコードの部分のみ返してください。\n"
     system_prompt = system_prompt1 + system_prompt2 + system_prompt3
 
-    #user prompt
-    prompt_user_request = request.data
-    #prompt_user_request = "画面中央にボタン1つ"
+    # user prompt
+    prompt_user_request = request
 
-    #output = translator.translate_text(system_prompt, target_lang=target_lang)
-    #print("翻訳結果\n" + output.text)
+    # 翻訳（日本語→英語）
+    system_prompt_en = translator.translate_text(system_prompt, source_lang=source_lang, target_lang=target_lang).text
+    prompt_user_request_en = translator.translate_text(prompt_user_request, source_lang=source_lang, target_lang=target_lang).text
 
+    # APIリクエスト
     response = client.chat.completions.create(
         model=model_name,
         messages=[
-            {"role": "system", "content": translator.translate_text(system_prompt, source_lang=source_lang, target_lang=target_lang).text},
-            {"role": "user", "content": translator.translate_text(prompt_user_request, source_lang=source_lang, target_lang=target_lang).text}
+            {"role": "system", "content": system_prompt_en},
+            {"role": "user", "content": prompt_user_request_en}
         ],
         n           = 1,                # 返答数
         temperature = 0,                # 出力する単語のランダム性（0から2の範囲） 0であれば毎回返答内容固定
     )
+
+    # 不要なレスポンスを加工
     response_all = response.choices[0].message.content
-    response_a = response_all[7:]
-    response_b = response_a[:-3]
-    return response_all
-
-#html_res = createPrompt('s')
-#print(html_res)
-
-
+    response_lstrip = response_all.lstrip('```html\n')
+    response_rstrip = response_lstrip.rstrip('```\n')
+    
+    return response_rstrip
